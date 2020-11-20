b4bb5f8900bf66a0a2bb822f23d69b8739a7115d
mis6us <mis6us@icloud.com>
2020-11-21 01:27:00 +0200

    Added code with solution of the problem

--- src/barracksWars/Main.java
+++ src/barracksWars/Main.java
@@ -6,10 +6,15 @@ import barracksWars.interfaces.UnitFactory;
 import barracksWars.core.Engine;
 import barracksWars.core.factories.UnitFactoryImpl;
 import barracksWars.data.UnitRepository;
+import blackBoxInteger.BlackBoxInt;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         Repository repository = new UnitRepository();
         UnitFactory unitFactory = new UnitFactoryImpl();
 
